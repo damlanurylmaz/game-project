71c7384d769558987e6e8e0177a47c2cf9edf14e
Damla Yilmaz <damlanur.ylmaz@hotmail.com>
2024-04-01 14:49:37 +0300

    Add inputs to profile form part

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,6 +1,7 @@
 import Header from '../../Components/Header'
 import { ProfileWrapper } from './Profile.styled'
 import vesikalik from '../../assets/pngs/vesikalik.jpg'
+import { TextField } from '@mui/material'
 
 const Profile = () => {
   return (
@@ -10,8 +11,47 @@ const Profile = () => {
         </div>
         <div className='profile-container'>
             <div className='profile-form'>
-                <div className='profile-image'>
-                    <img className='image' src={vesikalik} alt='Profile Image'/>
+                <div className='form-top-part'>
+                    <div className='profile-image'>
+                        <img className='image' src={vesikalik} alt='Profile Image'/>
+                    </div>
+                    <div className='name-input-container'>
+                        <h1>Profile Information</h1>
+                        <TextField
+                            id="outlined-read-only-input"
+                            defaultValue="Name"
+                            InputProps={{
+                                readOnly: true,
+                            }}
+                        />
+                        <TextField
+                            id="outlined-read-only-input"
+                            defaultValue="Surname"
+                            InputProps={{
+                                readOnly: true,
+                            }}
+                        />
+                    </div>
+                </div>
+                <div className='form-changeable-part'>
+                    <div className='changeable-input-container'>
+                        <TextField
+                            id="outlined-helperText"
+                            defaultValue="Enter new email address."
+                        />
+                    </div>
+                    <div className='changeable-input-container'>
+                        <TextField
+                            id="outlined-helperText"
+                            defaultValue="damlanur@hotmail.com"
+                            disabled
+                            type='password'
+                        />
+                        <TextField
+                            id="outlined-helperText"
+                            defaultValue="Enter new password."
+                        />
+                    </div>
                 </div>
             </div>
         </div>
--- src/Pages/Profile/Profile.styled.js
+++ src/Pages/Profile/Profile.styled.js
@@ -21,10 +21,47 @@ export const ProfileWrapper = styled.div`
             border: 1px solid rgba(108, 39, 255, 0.23);
             border-radius: 15px;
             padding: 50px;
-            .profile-image {
-                .image {
-                    width: 200px;
-                    border-radius: 50%;
+            .form-top-part {
+                display: flex;
+                align-items: center;
+                gap: 100px;
+                margin-bottom: 50px;
+                .profile-image {
+                    .image {
+                        width: 200px;
+                        border-radius: 50%;
+                    }
+                }
+                .name-input-container {
+                    display: flex;
+                    flex-direction: column;
+                    gap: 30px;
+                    .MuiInputBase-root  {
+                        background-color: #FFEFD0;
+                        width: 300px;
+                        border-radius: 15px;
+                        outline: none;
+                        border: none;
+                    }
+                }
+            }
+            .form-changeable-part {
+                width: 100%;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                flex-direction: column;
+                gap: 50px;
+                .changeable-input-container {
+                    display: flex;
+                    gap: 50px;
+                    .MuiInputBase-root  {
+                        background-color: #FFEFD0;
+                        width: 300px;
+                        border-radius: 15px;
+                        outline: none;
+                        border: none;
+                    }
                 }
             }
         }
