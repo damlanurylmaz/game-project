6b9e2b7b50588c46fb9a9d151980b25f43b1eec9
Damla Yilmaz <damlanur.ylmaz@hotmail.com>
2024-04-19 08:31:50 +0300

    Fixed style_default error

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    jsxImportSource: '@emotion/react',
-    babel: {
-      plugins: ['@emotion/babel-plugin'],
-    },
-  }),],
+  optimizeDeps: {
+    include: [
+      '@emotion/react', 
+      '@emotion/styled', 
+      '@mui/material/Tooltip'
+    ],
+  },
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+  ],
 })
